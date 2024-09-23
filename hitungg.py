c893bf41ec974a0a9d327792e1deb3a7f1fc4ce2
Syafiraagiez <syafira.agiez.2203226@students.um.ac.id>
2024-09-23 10:14:44 +0700

    Update hitungg.py

--- hitungg.py
+++ hitungg.py
@@ -5,10 +5,10 @@ sx = st.text_input("Satuan C", "C")
 st.write ("Anda memasukkan", x, ' ',sx)
 sy = st.text_input("Dikonversi ke", "C")
 y = 0
-if (sx == 'C') ;
-  if(sy == 'C') ;
+if (sx == 'C');
+  if(sy == 'C');
     y = x
   elif(sy == 'F'):
-    (9/5) * x + 32
+   F = (9/5) * x + 32
 
 st.write (x,' ',sx, ' = ',y, sy)
