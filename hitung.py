39f7768112ab69744df771b78b3fa7677144647a
Syafiraagiez <syafira.agiez.2203226@students.um.ac.id>
2024-09-23 09:47:32 +0700

    Create hitung.py

---
+++ hitung.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+x = st.numer_input(
+  "insert a number", value=None, placeholder="Type a number..."
+)
+st.write("The current number is ", x)
+st.latex(r'''
+    x^2 =
+    ''')
+st.write(x*x)
