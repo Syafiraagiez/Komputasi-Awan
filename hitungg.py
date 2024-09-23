673c69440defc7beec3bd58639d65ac6f2f5292c
Syafiraagiez <syafira.agiez.2203226@students.um.ac.id>
2024-09-23 10:29:07 +0700

    Update hitungg.py

--- hitungg.py
+++ hitungg.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 x = st.number_input("Masukan Angka")
-sx = st.text_input("Satuan C", "C")
+sx = st.text_input("Satuan", "C")
 st.write ("Anda memasukkan", x, ' ',sx)
 sy = st.text_input("Dikonversi ke", "C")
 y = 0
