fdbf450ec898f0570e271949e77f49076bb6b26f
Syafiraagiez <syafira.agiez.2203226@students.um.ac.id>
2024-09-23 10:12:25 +0700

    Update hitungg.py

--- hitungg.py
+++ hitungg.py
@@ -4,22 +4,11 @@ x = st.number_input("Masukan Angka")
 sx = st.text_input("Satuan C", "C")
 st.write ("Anda memasukkan", x, ' ',sx)
 sy = st.text_input("Dikonversi ke", "C")
-if(Satuan == "C"):
-  if(konversi == "C"):
-   print(x, " C = ", x, "C")
-  elif(konversi == "F"):
-    y = x*9/5+32
-    print(x, " C = ", y,"R")
-  elif(konversi == "K"):
-    y = x*9/5+32
-    print(x, " C = ", y,"K")
-  else:
-    print("Maaf, satuan konversi tidak di")
+y = 0
+if (sx == 'C');
+  if(sy == 'C');
+    y = x
+  elif(sy == 'F'):
+    (9/5) * x + 32
 
-elif(satuan == "F"):
-  if(konversi == "R"):
-    y = (x-32)*4/9
-    print(x, "F = ", y,"R")
-else:
-  print("Maaf, satuan tidak dikenali")
-st.write (x, ' ',sx, '= ...', sy)
+st.write (x,' ',sx, '= ',y, sy)
