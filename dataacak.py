b88412217e4bcb25682f89f4515e565a0cb9c43f
Syafiraagiez <syafira.agiez.2203226@students.um.ac.id>
2024-10-24 09:17:28 +0700

    Update and rename datacak.py to dataacak.py

--- dataacak.py
+++ dataacak.py
@@ -6,7 +6,7 @@ from matplotlib.patches import Circle
 
 
 st.title("Fisika Komputasi Awan")
-st.title("Syafira Agiez Faradizca :sunglasses:")
+st.title("Syafira Agiez Faradizca :monocle:")
 circle = Circle((0, 0), 1, color='red', fill=False, linewidth=2, linestyle='-', alpha=0.2)
 x = []
 y = []
@@ -52,7 +52,4 @@ ax.set_ylim([-1, 1])
 st.pyplot(fig)
 st.caption("Lingkaran dengan ukuran dan warna acak dan tersebar didalam lingkaran dengan radius 1")
 st.divider()
-st.text("Buat website seperti di atas dengan menggunakan GitHub dan Streamlit.")
-st.text("Kirim video saat website dijalankan.") 
-st.text("Pastikan nama dan alamat website terlihat.")
-st.divider()
+
